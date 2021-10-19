06d544adee6883d4172250678fc8135c5561319b
De Wet Oosthuizen <dewetharm@gmail.com>
2021-10-19 19:23:58 +0100

    Initial commit

---
+++ Timetables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Timetables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
