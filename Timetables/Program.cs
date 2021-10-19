b4b5544214403a0576061a9c3300d29820d2dfe9
De Wet Oosthuizen <dewetharm@gmail.com>
2021-10-19 20:01:41 +0100

    Added initial code

--- Timetables/Program.cs
+++ Timetables/Program.cs
@@ -6,7 +6,20 @@ namespace Timetables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome user and ask which times table they would like to view
+            Console.WriteLine("Welcome to Multiply-till-YouFly");
+            Console.WriteLine("Which times table would you like to see?");
+            string userChoice = Console.ReadLine();
+
+            // Display the user's choice in a message before output is printed
+            Console.WriteLine($"You have selected the {userChoice} times table");
+
+            // Display the time table using the user's choice
+            int table = int.Parse(userChoice);
+            for (int i = 1; i <= 12; i++)
+                {
+                    Console.WriteLine($"{i} x {table} = {i * table}");
+                }
         }
     }
 }
